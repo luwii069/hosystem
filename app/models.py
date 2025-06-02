45cfe0b737a54feac883da6522686ec1ca2ee36f
louis mulaa <louismulaa06@gmail.com>
2025-06-02 16:45:54 +0300

    added the patients model

--- app/models.py
+++ app/models.py
@@ -10,3 +10,26 @@ class Admin (db.model):
 
     def __repr__(self):
         return f"Admin('{self.username}')"
+
+class Patient(db.model):
+    __tablename__ ="patients"  
+     
+    id = db.Column(db.BigInteger, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    admn_no = db.Column(db.String(20), unique=True, nullable=False)
+    phone = db.Column(db.String(15))
+    gender = db.Column(db.String(10))
+    date_of_birth = db.Column(db.Date)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    created_by_admin_id = db.Column(db.Integer, db.ForeignKey('admins.id'))
+    created_by_admin = db.relationship('Admin', foreign_keys=[created_by_admin_id])
+
+    appointments = db.relationship('Appointment', backref='patient', lazy=True)
+    medical_records = db.relationship('MedicalRecord', backref='patient', lazy=True)
+
+    def __repr__(self):
+        return f"Patient('{self.name}', '{self.email}', '{self.admn_no}')"
+ 
+
